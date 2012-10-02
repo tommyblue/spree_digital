bbaff29c795736d4ae6a6e55db7f5e59ec64cb56
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-10-02 16:53:41 +0200

    Modifica alle migrazioni
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

--- db/migrate/20121002163604_modify_digitals.rb
+++ db/migrate/20121002163604_modify_digitals.rb
@@ -1,6 +1,6 @@
-class ModifiyDigitals < ActiveRecord::Migration
+class AddFileUrlToDigitals < ActiveRecord::Migration
 
-  def self.up
+  def change
     add_column :spree_digitals, :file_url, :string
 
     remove_column :spree_digitals, :attachment_file_name
@@ -8,12 +8,6 @@ class ModifiyDigitals < ActiveRecord::Migration
     remove_column :spree_digitals, :attachment_file_size
 
     add_index :spree_digitals, :file_url
-  end
-
-  def self.down
-    add_column :spree_digitals, :attachment_file_name, :string
-    add_column :spree_digitals, :attachment_content_type, :string
-    add_column :spree_digitals, :attachment_file_size, :integer
   end
 
 end
