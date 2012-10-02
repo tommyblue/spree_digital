48450770dd7437a883ac2aa6028af67f8dc89e9a
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-10-02 17:19:30 +0200

    Viste
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

--- app/controllers/spree/digitals_controller.rb
+++ app/controllers/spree/digitals_controller.rb
@@ -1,27 +1,20 @@
 module Spree
   class DigitalsController < Spree::BaseController
     ssl_required :show
-  
+
     def show
       link = DigitalLink.find_by_secret(params[:secret])
 
-      if link.present? and link.digital.attachment.present?
-        attachment = link.digital.attachment
+      if link.present? and link.digital.file_url.present?
+        attachment = link.digital
 
-        # don't authorize the link unless the file exists
-        # the logger error will help track down customer issues easier
-
-        if File.file?(attachment.path)
-          if link.authorize!
-            send_file attachment.path, :filename => attachment.original_filename, :type => attachment.content_type and return
-          end
-        else
-          Rails.logger.error "Missing Digital Item: #{attachment.path}"
+        if link.authorize!
+          redirect_to attachment.file_url and return
         end
       end
-      
+
       render :unauthorized
     end
-    
+
   end
 end
